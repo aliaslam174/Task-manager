160e86efe4c7241ee0b3588ef469313c32234059
Muhammad Ali <122381911+aliaslam174@users.noreply.github.com>
2024-10-28 14:50:26 +0500

    update home

--- src/compnent/Home.jsx
+++ src/compnent/Home.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
-function Home() {
+function Home({children}) {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
+  // Redirect to login if not authenticated
   if (!isAuthenticated) {
-    return <Navigate to="/login" />; // Redirect to login if not authenticated
+    return <Navigate to="/login" />;
   }
 
-  return (
-    <>
-      <h1 align="center" className='text-3xl font-bold mt-5'>Task Manager Application</h1>
-      {/* Other components or content can go here */}
-    </>
-  );
+  // Redirect to dashboard if authenticated
+  return <Navigate to="/dashboard" />;
 }
 
 export default Home;
